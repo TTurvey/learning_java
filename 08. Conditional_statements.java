920d8bc76019acd7a18ef3e63ff080a883792cdd
Tim Turvey <tturvey@hotmail.co.uk>
2022-05-07 16:39:44 +0100

    Conditional statements

---
+++ 08. Conditional_statements.java
@@ -0,0 +1,14 @@
+// Conditional Statements and Comparison Operators
+
+// The if statement executes a block of code when a specified boolean expression is evaluated as true.
+// The if-else statment. If the previous statment is false then evaluate condition and execute code if true, else move on.
+// The else statement. The else statement is always the last condition.
+
+// The if-else statement.
+if (expression) {
+  // Code to run if expression is true
+} else if (expression) {
+  // Code to run if previous expression is false and current condition is true
+} else {
+  // Code to run if all previous expressions are false
+}
